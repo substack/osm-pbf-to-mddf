f2a71c96fbc4925e49562eea3bb71675c56788e6
James Halliday <mail@substack.net>
2015-03-26 13:59:26 +1300

    blah

--- index.js
+++ index.js
@@ -10,37 +10,37 @@ module.exports = function (df) {
     
     osm.pipe(through.obj(write));
     return writeonly(osm);
-};
-
-function write (ilist, enc, next) {
-    var pending = 0;
-    function advance () { if (--pending === 0) next() }
     
-    for (var i = 0; i < ilist.length; i++) {
-        var item = ilist[i];
-        if (item.type === 'node') {
-            items[item.id] = ecef(item.lat, item.lon);
-        }
-        else if (item.type === 'way') {
-            var len = item.refs.length;
-            var data = Buffer(len * 3 * 4);
-            center[0] = 0;
-            center[1] = 0;
-            center[2] = 0;
-            
-            for (var j = 0; j < len; j++) {
-                var r = items[item.refs[j]];
-                center[0] += r[0] / len;
-                center[1] += r[1] / len;
-                center[2] += r[2] / len;
-                
-                data.writeFloatBE(r[0], j*4*3+0);
-                data.writeFloatBE(r[1], j*4*3+4);
-                data.writeFloatBE(r[2], j*4*3+8);
+    function write (ilist, enc, next) {
+        var pending = 0;
+        function advance () { if (--pending === 0) next() }
+        
+        for (var i = 0; i < ilist.length; i++) {
+            var item = ilist[i];
+            if (item.type === 'node') {
+                items[item.id] = ecef(item.lat, item.lon);
             }
-            pending ++;
-            df.put(center, data, advance);
+            else if (item.type === 'way') {
+                var len = item.refs.length;
+                var data = Buffer(len * 3 * 4);
+                center[0] = 0;
+                center[1] = 0;
+                center[2] = 0;
+                
+                for (var j = 0; j < len; j++) {
+                    var r = items[item.refs[j]];
+                    center[0] += r[0] / len;
+                    center[1] += r[1] / len;
+                    center[2] += r[2] / len;
+                    
+                    data.writeFloatBE(r[0], j*4*3+0);
+                    data.writeFloatBE(r[1], j*4*3+4);
+                    data.writeFloatBE(r[2], j*4*3+8);
+                }
+                pending ++;
+                df.put(center, data, advance);
+            }
         }
+        if (pending === 0) next();
     }
-    if (pending === 0) next();
-}
+};
